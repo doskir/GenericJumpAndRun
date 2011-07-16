f137bcfc377c0c6e3aab7e363be269ddd7a3af2e
Patrick Speiser <doskir@gmail.com>
2011-07-16 15:36:23 +0200

    Initial commit.

--- GenericJumpAndRun/GenericJumpAndRun/Level.cs
+++ GenericJumpAndRun/GenericJumpAndRun/Level.cs
@@ -1,11 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace GenericJumpAndRun
 {
     class Level
     {
+        public List<GameObject> GameObjects;
+        public Level()
+        {
+            GameObjects = new List<GameObject>();
+        }
     }
 }
