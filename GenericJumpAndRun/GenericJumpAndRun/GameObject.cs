0fa98d315095ae511e078fbf100afc91fba52a98
Patrick Speiser <doskir@gmail.com>
2011-07-16 13:46:53 +0200

    Added Generic game object, all other objects should inherit this, also added an empty player class.

---
+++ GenericJumpAndRun/GenericJumpAndRun/GameObject.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GenericJumpAndRun
+{
+    class GameObject
+    {
+        public Vector2 Position
+        {
+            get { return _position; }
+            set
+            {
+                _position = value;
+                BoundingRectangle.X = (int) _position.X;
+                BoundingRectangle.Y = (int) _position.Y;
+            }
+        }
+        private Vector2 _position;
+        public Vector2 Velocity;
+        public readonly Texture2D Sprite;
+        public BoundingRectangle BoundingRectangle;
+        public GameObject(Vector2 position,Vector2 velocity,Texture2D sprite)
+        {
+            _position = position;
+            Velocity = velocity;
+            Sprite = sprite;
+            BoundingRectangle = new BoundingRectangle((int) _position.X, (int) _position.Y, sprite.Width, sprite.Height);
+        }
+
+        public bool IntersectsWith(GameObject gameObject)
+        {
+            if(BoundingRectangle.IntersectsWith(gameObject.BoundingRectangle))
+            {
+                //TODO: Implement higher precision collision detection
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ GenericJumpAndRun/GenericJumpAndRun/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GenericJumpAndRun
+{
+    class Player : GameObject
+    {
+        public Player(Vector2 position, Vector2 velocity, Texture2D sprite) : base(position, velocity, sprite)
+        {
+            
+        }
+    }
+}
