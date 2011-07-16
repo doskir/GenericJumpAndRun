3325314bd79c656b2e32433b45c840e8212d2977
Patrick Speiser <doskir@gmail.com>
2011-07-16 15:37:27 +0200

    Added Movement,Jumping and Falling should be fine for now.

--- GenericJumpAndRun/GenericJumpAndRun/Player.cs
+++ GenericJumpAndRun/GenericJumpAndRun/Player.cs
@@ -13,5 +13,70 @@ namespace GenericJumpAndRun
         {
             
         }
+
+        private Vector2 _lastPosition;
+        private Vector2 _lastVelocity;
+        public bool CanJump = true;
+        public void Jump()
+        {
+            if(CanJump)
+            {
+                Velocity.Y -= 10f;
+                CanJump = false;
+            }
+        }
+        public void Update(Level currentLevel)
+        {
+            _lastPosition = Position;
+            _lastVelocity = Velocity;
+            if (Velocity.Y > 0)
+                Position += new Vector2(Velocity.X, 0);
+            else
+                Position += Velocity;
+            Velocity.X *= 0.9f;
+            //going up
+            if (Velocity.Y < 0)
+                Velocity.Y *= 0.9f;
+            if (IntersectsWithAny(currentLevel.GameObjects))
+            {
+                UndoLastMove();
+                Velocity = Vector2.Zero;
+            }
+            Fall(currentLevel);
+        }
+        public enum Direction
+        {
+            Left,Right
+        }
+        public void Move(Direction movementDirection)
+        {
+            if (movementDirection == Direction.Left)
+                Velocity.X -= 0.5f;
+            else if (movementDirection == Direction.Right)
+                Velocity.X += 0.5f;
+        }
+
+        public void UndoLastMove()
+        {
+            Position = _lastPosition;
+            Velocity = _lastVelocity;
+        }
+        public void Fall(Level level)
+        {            //gravity
+            Vector2 originalPosition = Position;
+            Velocity.Y += 0.1f;
+            Vector2 verticalVelocity = new Vector2(0, Velocity.Y);
+            Position += verticalVelocity;
+            if(IntersectsWithAny(level.GameObjects))
+            {
+                Position = originalPosition;
+                ReachedBottom();
+            }
+        }
+        public void ReachedBottom()
+        {
+            Velocity.Y = 0;
+            CanJump = true;
+        }
     }
 }
